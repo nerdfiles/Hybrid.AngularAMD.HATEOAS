f2f2b9b7a5f7bf0e5a5833ead2131f4df5fe41d7
nerdfiles <nerdfiles@gmail.com>
2017-05-20 19:39:17 -0500

    'Update'

--- client/scripts/utils/google.maps.js
+++ client/scripts/utils/google.maps.js
@@ -1,7 +1,15 @@
+// ./client/scripts/utils/google.maps.js
+/**
+ * @ngdoc overview
+ * @name app.utils.google.maps:base
+ * @description Google Maps API utility.
+ */
 define([
   'async!//maps.google.com/maps/api/js?v=3&sensor=false'
 ], base);
 function base () {
+  'use strict';
+
   return angular
     // Grouping common Google Maps stuffs
     .module('common', [])
