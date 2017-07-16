810a19103046b765ff43b3b6d761e9c2bdc5ce89
nerdfiles <nerdfiles@gmail.com>
2017-07-16 15:58:02 -0500

    Add schema for form input repeater

--- client/app/directive/form/index.js
+++ client/app/directive/form/index.js
@@ -1,8 +1,42 @@
+/**
+ * @ngdoc directive
+ */
 define([
   'interface'
 ], function(__interface__) {
-  __interface__.directive('🔤', [__Form__])
-  function __Form__() {
+  'use strict'
+
+  var directiveInterface = [
+    '$rootScope',
+    __Form__
+  ]
+
+  __interface__.directive('🔤', directiveInterface)
+
+  /**
+   * @name __Form__
+   * @schema
+   *   form[
+   *     [
+   *       {
+   *         id: 'name',
+   *         label: 'Some name',
+   *         name: 'name',
+   *         type: 'text',
+   *         value: '',
+   *         placeholder: ''
+   *       }
+   *     ]
+   *   ]
+   * @usage
+   *   <section
+   *     🔤
+   *     form="vm.form"
+   *   >
+   *   </section>
+   * @returns {object} AngularJS directive factory.
+   */
+  function __Form__($rootScope) {
     var dv = {
       scope: {
         form: '='
